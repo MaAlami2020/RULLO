5df24027b415fc697e118025c0dba2a4fe441bd2
MaAlami2020 <96190903+MaAlami2020@users.noreply.github.com>
2021-12-15 23:46:38 +0100

    Update rullo.pas
    de momento funciona al compilar, sin embargo:
    - me preocupa el case del procedure, no se si es la mejor forma de implementarlo igual al finalizar se encuentre una manera de hacer  el código más sencillo,
    -  de momento el procedure JuegoDelRullo funciona solo 1 vez, así que había pensado en poner un bucle hasta que pulse la 4ª opcionMenu llamada salir,
    - creo que  se trata de un array de TCeldas y me da que el tamaño  del array será de 50 para la matriz 5*5,  72 para la de 6*6 y 98 para la de 7*7 ya que el valor del sumatorio (el de la caja en el juego original) para  un mismo valor de visible (el nº de la celda en el juego original) variará en función de la fila y la columna; pero no loo sé del todo,
    - en la función "SeleccionarDimension" he puesto opcionDimension=chr(65) porque por alguna razón no me verifica  que la opción de la dimensión de la tabla es 'A' pero con esta opción me va bien.

--- rullo.pas
+++ rullo.pas
@@ -1,87 +1,222 @@
 PROGRAM rullo;
-PROCEDURE Dimension;
+CONST
+  MIN=1;
+  MAX=7;
+TYPE
+   TLimite=MIN..MAX;
+   TCelda=RECORD
+     visible:integer;
+     sumable:integer;
+     activado:boolean;
+   end;
+
+   TTabla=ARRAY[TLimite,TLimite]OF TCelda;
+
+PROCEDURE DimensionesDisponibles;
 //es una función que mostrará por pantalla las dimensiones diponibles para este juego
 BEGIN
+  WRITELN(' DIMENSIONES DISPONIBLES');
   WRITELN('a. 5x5');
   WRITELN('b. 6x6');
   WRITELN('c. 7x7');
 END;
 
-PROCEDURE RangoDeCifras;
+PROCEDURE RangoDeCifrasDisponibles;
 BEGIN
+  WRITELN(' RANGO DE CIFRAS DISPONIBLES');
   WRITELN('a. 1-9');
   WRITELN('b. 1-19');
 END;
 
 PROCEDURE Ayuda;
-//la ayuda no está completamente finalizada porque habrá que indicarle al usuario que opción pulsar para interrumpir una partida 
+//la ayuda no está completamente finalizada porque habrá que indicarle al usuario que opción pulsar para interrumpir una partida
 BEGIN
+
   WRITELN('El juego consiste en que la suma de los numeros de cada fila o columna  debe ser igual al numero de la caja');
   WRITELN('usted debe clickar sobre un numero para excluirlo de la suma');
 
 END;
 
-PROCEDURE Tabla(VAR opcionDimension,opcionRango:char);
-//en esta función se seleccionará el tamaño de la tabla del juego, esto se repetirá hasta que seleccione una opción válid; cuando la opción ya sea válida se seleccionará el rango 1-9 o 1-19 y al igual que antes se repetirá hasta que haya hecho la selección correcta
-//por otra parte, en principio, he puesto estos parámetros de salida para usarlos en una nueva funciónn que llamaré IniciarTabla; pero no es definitivo, tengo que probarlo
+FUNCTION VerificarDimension(opcionDimension:char):boolean;
+BEGIN
+
+  IF(opcionDimension=chr(65))OR(opcionDimension=chr(66))OR(opcionDimension=chr(67))THEN
+     VerificarDimension:=TRUE
+  ELSE
+     VerificarDimension:=FALSE;
+
+END;
+
+FUNCTION SeleccionarDimension(opcionDimension:char):char;
 BEGIN
 
   REPEAT
-    Dimension;
+    DimensionesDisponibles;
     WRITELN;
     WRITELN('Selecciona la dimension: a, b o c');
     READLN(opcionDimension);
-    IF(opcionDimension<>'a')OR(opcionDimension<>'b')OR(opcionDimension<>'c')THEN
+    opcionDimension:=UPCASE(opcionDimension);
+    IF NOT VerificarDimension(opcionDimension) THEN
         WRITELN('seleccion erronea, pruebe otra vez');
-  until(opcionDimension='a')OR(opcionDimension='b')OR(opcionDimension='c');
+  until(opcionDimension=chr(65))OR(opcionDimension=chr(66))OR(opcionDimension=chr(67));
+
+  SeleccionarDimension:=opcionDimension;
+
+END;
+
+FUNCTION VerificarRango(opcionRango:char):boolean;
+BEGIN
+
+  IF(opcionRango=chr(65))OR(opcionRango=chr(66))THEN
+     VerificarRango:=TRUE
+  ELSE
+     VerificarRango:=FALSE;
+
+END;
+
+FUNCTION SeleccionarRango(opcionRango:char):char;
+BEGIN
 
   REPEAT
-    RangoDeCifras;
+    RangoDeCifrasDisponibles;
     WRITELN;
     WRITELN('Selecciona el rango de cifras: a o b');
     READLN(opcionRango);
-    IF(opcionRango<>'a')OR(opcionRango>'b')THEN
+    opcionRango:=UPCASE(opcionRango);
+    IF NOT VerificarRango(opcionRango) THEN BEGIN
         WRITELN('seleccion erronea, pruebe otra vez');
-  until(opcionRango='a')OR(opcionRango='b');
+        WRITELN;
+    end;
+  until(opcionRango=chr(65))OR(opcionRango=chr(66));
+
+  SeleccionarRango:=opcionRango;
+
+END;
+
+
+PROCEDURE InicializarTabla(VAR tabla:TTabla;dim,rango:integer);
+VAR
+  i,j:integer;
+BEGIN
+
+  FOR i:=MIN TO dim DO BEGIN
+    FOR j:=MIN TO dim DO
+    //el random pone los valores o del 1 al 9 o del 1 al 19
+       tabla[i,j].visible:=RANDOM(rango)+1;
+       tabla[i,j].sumable:=RANDOM(rango)+1;
+      // tabla[i,j].activado:=boolean;
+  end;
+
+END;
+
+PROCEDURE MostrarTabla(tabla:TTabla;dim:integer);
+VAR
+  i,j:integer;
+BEGIN
+
+  FOR i:=MIN TO dim DO BEGIN
+    FOR j:=MIN TO dim DO BEGIN
+       WRITE(tabla[i,j].visible:5);
+    end;
+    WRITELN;
+  end;
+
+END;
+
+PROCEDURE Jugar(VAR tabla:TTabla);
+VAR
+  opcionDimension,opcionRango:char;
+  valorDim,valorRango:integer;
+BEGIN
+//en esta función se llamara a 2 funciones, una que devolvera el tamaño de la tabla del juego, esto se repetirá hasta que seleccione una opción válida; cuando la opción ya sea válida se llamará a la funcion para obtener el rango 1-9 o 1-19 y al igual que antes se repetirá hasta que haya hecho la selección correcta
+//cuando se salga de cada una de las funciones se almacenara el valor que devuelve en variable locales "opcionDimension" y "opcionRango"
+    opcionDimension:=SeleccionarDimension(opcionDimension);
+    opcionRango:=SeleccionarRango(opcionRango);
+    CASE opcionRango OF
+       'A':BEGIN
+//si se selecciono 'a' o 'A' de dimension y 'a' o 'A' de rango se inicializza la tabla de celdas con valores aleatorios (falta revisar)
+            IF(opcionDimension='A')THEN BEGIN
+                valorDim:=5;valorRango:=9;
+                InicializarTabla(tabla,valorDim,valorRango);
+                MostrarTabla(tabla,valorDim);
+            END;
+            IF(opcionDimension='B')THEN BEGIN
+                valorDim:=6;valorRango:=9;
+                InicializarTabla(tabla,valorDim,valorRango);
+                MostrarTabla(tabla,valorDim);
+            END;
+            IF(opcionDimension='C')THEN BEGIN
+                valorDim:=7;valorRango:=9;
+                InicializarTabla(tabla,valorDim,valorRango);
+                MostrarTabla(tabla,valorDim);
+            END;
+        END;
+        'B':BEGIN
+            IF(opcionDimension='A')THEN BEGIN
+                valorDim:=5;valorRango:=19;
+                InicializarTabla(tabla,valorDim,valorRango);
+                MostrarTabla(tabla,valorDim);
+            END;
+            IF(opcionDimension='B')THEN BEGIN
+                valorDim:=6;valorRango:=19;
+                InicializarTabla(tabla,valorDim,valorRango);
+                MostrarTabla(tabla,valorDim);
+            END;
+            IF(opcionDimension='C')THEN BEGIN
+                valorDim:=7;valorRango:=19;
+                InicializarTabla(tabla,valorDim,valorRango);
+                MostrarTabla(tabla,valorDim);
+            END;
+        END;
+    end;{CASE}
 
 END;
 
 PROCEDURE Menu;
 //con esta función se muestra por pantalla las opciones disponibles antes de jugar, aparecerá nada más entrar en la app
 BEGIN
+  WRITELN(' MENU ');
   WRITELN('1.Obtener ayuda');
   WRITELN('2.Seleccionar la dimension');
   WRITELN('3.Leer ranking');
+  WRITELN('4.Salir');
 END;
 
 PROCEDURE JuegoDelRullo;
 //el usuario seleccionará una de las tres opciones del menu, esto se repetirá tantas veces hasta que el usuario seleccione una opción del menú, después será un case
 VAR
   opcionMenu:integer;
+  tabla:TTabla;
 BEGIN
 
-  WRITELN('! BIENVENIDO AL JUEGO DEL RULO ¡');
+  WRITELN('!!! BIENVENIDO AL JUEGO DEL RULO !!!');
   WRITELN;
-  WRITELN('MODO CLASICO');
+  WRITELN(' MODO CLASICO ');
   WRITELN;
+
   REPEAT
      Menu;
      WRITELN;
      WRITELN('seleccione una opcion del menu: 1, 2 o 3');
      READLN(opcionMenu);
-     IF(opcionMenu<1)OR(opcionMenu>3)THEN
+     IF(opcionMenu<1)OR(opcionMenu>3)THEN BEGIN
         WRITELN('seleccion erronea, pruebe otra vez');
+        WRITELN;
+     end;
   until(opcionMenu>=1)AND(opcionMenu<=3);
+
   CASE opcionMenu OF
      1:Ayuda;
-     2:Tabla;
-     3:Ranking;
+     2:Jugar(tabla);
+    // 3:Ranking;
+    // 4:WRITELN('HASTA PRONTO!');
   END;
 
 END;
 
 BEGIN
   RANDOMIZE;
+//he declarado como variable local dentro del procedure JuegoDelRullo a 'tabla' en vez de pasarlo como parametro porque he considerado que fuera del subprograma su valor no interesa
   JuegoDelRullo;
 READLN;
-END. 
+END.  
