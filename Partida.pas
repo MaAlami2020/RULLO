2186dded6b421d7179051a510cbf11b1cb02ed8c
Cristian1A <96200703+Cristian1A@users.noreply.github.com>
2021-12-15 21:06:03 +0100

    Sugerencia
    Función que imprime por pantalla la tabla de las modalidades de juego y recoge el modo escogido.

---
+++ Partida.pas
@@ -0,0 +1,21 @@
+PROGRAM Partida;
+  USES crt;
+  {Muestra los tipos de partidas y recoge la escogida}
+  FUNCTION ElegirPartida:char;
+    VAR c: char;
+    BEGIN
+      textcolor(lightred);
+      REPEAT
+        WRITELN('     5x5  6x6  7x7');
+        WRITELN(' 1-9  A    B    C');
+        WRITELN('1-19  D    E    F');
+        WRITE('Escriba el tipo de partida que quiere jugar ');c:= readkey;
+        CLRSCR;
+      UNTIL(c='a') or (c='b') or (c='c') or (c='d') or (c='e') or (c='f');
+      ElegirPartida:= c;
+    END;
+
+BEGIN
+  WRITELN(ElegirPartida);
+  READLN;
+END.
